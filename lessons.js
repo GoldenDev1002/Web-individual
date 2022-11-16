4121c661bce4d52f23139934dc99a8f9f9cb59b8
GoldenDev1002 <jenzielol1002@gmail.com>
2022-11-16 17:34:57 +0000

    updated lessons js

--- lessons.js
+++ lessons.js
@@ -1,91 +1,90 @@
 
     export const lessons = [
-        {id: 1001,
-          subject: "English",
-          location: "Hendon",
-          price: 1000,
-          spaces: 5,
-          img: "subjectPic/english.jpg"
-          
-        },
-    
-        {id: 1002,
-          subject: "History",
-          location: "Leeds",
-          price: 2000,
-          spaces: 5,
-          img: "subjectPic/history.jpg"
-        },
-    
-        {  id: 1003,
-          subject: "Literature",
-          location: "Manchester",
-          price: 2500,
-          spaces: 5,
-          img: "subjectPic/literature.jpg"
-        },
-        {id: 1004,
-          subject: "Biology",
-          location: "Hendon",
-          price: 2500,
-          spaces: 5,
-          img: "subjectPic/biology.jpg"
-        },
-    
-        {
-          id: 1005,
-          subject: "Chemistry",
-          location: "Leeds",
-          price: 2400,
-          spaces: 5,
-          img: "subjectPic/chemistry.jpg"
-        },
-    
-        {
-          id: 1006,
-          subject: "Physics",
-          location: "Hendon",
-          price: 2500,
-          spaces: 5,
-          img: "subjectPic/physics.png"
-        },
-    
-        {
-          id: 1007,
-          subject: "Economics",
-          location: "Hendon",
-          price: 3000,
-          spaces: 5,
-          img: "subjectPic/economics.jpg"
-        },
-  
-  
-        {
-          id: 1008,
-          subject: "Chemistry",
-          location: "Leeds",
-          price: 3000,
-          spaces: 5,
-          img: "subjectPic/chemistry.jpg"
-        },
-  
-        {id: 1009,
-          subject: "Biology",
-          location: "Hendon",
-          price: 2400,
-          spaces: 5,
-          img: "subjectPic/biology.jpg"
-        },
-  
+      {id: 1001,
+        subject: "English",
+        location: "Hendon",
+        price: 1000,
+        spaces: 5,
+        img: "subjectPic/english.jpg"
         
-        {  id: 1010,
-          subject: "Literature",
-          location: "Manchester",
-          price: 2500,
-          spaces: 5,
-          img: "subjectPic/literature.jpg"
-        
-        },
-    
-    ]
+      },
   
+      {id: 1002,
+        subject: "History",
+        location: "Leeds",
+        price: 2000,
+        spaces: 5,
+        img: "subjectPic/history.jpg"
+      },
+  
+      {  id: 1003,
+        subject: "Literature",
+        location: "Manchester",
+        price: 2500,
+        spaces: 5,
+        img: "subjectPic/literature.jpg"
+      },
+      {id: 1004,
+        subject: "Biology",
+        location: "Hendon",
+        price: 2500,
+        spaces: 5,
+        img: "subjectPic/biology.jpg"
+      },
+  
+      {
+        id: 1005,
+        subject: "Chemistry",
+        location: "Leeds",
+        price: 2400,
+        spaces: 5,
+        img: "subjectPic/chemistry.jpg"
+      },
+  
+      {
+        id: 1006,
+        subject: "Physics",
+        location: "Hendon",
+        price: 2500,
+        spaces: 5,
+        img: "subjectPic/physics.png"
+      },
+  
+      {
+        id: 1007,
+        subject: "Economics",
+        location: "Hendon",
+        price: 3000,
+        spaces: 5,
+        img: "subjectPic/economics.jpg"
+      },
+
+
+      {
+        id: 1008,
+        subject: "Chemistry",
+        location: "Leeds",
+        price: 3000,
+        spaces: 5,
+        img: "subjectPic/chemistry.jpg"
+      },
+
+      {id: 1009,
+        subject: "Biology",
+        location: "Hendon",
+        price: 2400,
+        spaces: 5,
+        img: "subjectPic/biology.jpg"
+      },
+
+      
+      {  id: 1010,
+        subject: "Literature",
+        location: "Manchester",
+        price: 2500,
+        spaces: 5,
+        img: "subjectPic/literature.jpg"
+      
+      },
+  
+  ]
