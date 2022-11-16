43009dc754d6d0247182aa7c03c1a7d44aede98b
GoldenDev1002 <jenzielol1002@gmail.com>
2022-11-16 17:17:26 +0000

    adding the sort and fileter feature of the code

--- main.js
+++ main.js
@@ -30,7 +30,13 @@ let app = new Vue({
         numberInput: "",
         message: "",
         search : "",
-        sortFeature: "subject"
+        sortFeature: "",
+        sortOption: "",
+        ascending : "",
+        descending : ""
+
+       
+      
       };
     },
 filters: {
@@ -38,7 +44,20 @@ filters: {
 },
     methods: {
       addToCart(aLesson) {
-        return this.cart.push(aLesson.spaces--);
+        // reduces number of spaces for lesson given
+        aLesson.spaces--;
+
+        // check if lesson passed is already existing in cart
+        var existingItemInCart = this.cart.find(item => item.lessonId === aLesson.id);
+        if(existingItemInCart == null){
+          // lesson does not yet exist in cart, add lesson to cart
+          // create and save cartItem object for new lesson to be added to cart
+          var cartItem = {lessonId : aLesson.id, spaces: 1, lesson: aLesson};
+          this.cart.push(cartItem);
+        }else{
+          // update number of spaces for existing lesson in cart
+          ++existingItemInCart.spaces;
+        }
       },
 
     
@@ -60,20 +79,121 @@ filters: {
 
         return count;
       },
-      checkBtn(){
-        let str = /[a-zA-Z]/ 
-        let phoneNumber = /[0=9]{11}/
-        if(this.nameInput.length != 0 && this.numberInput.length != 0){
-           this.message = "Complete"
 
-   
-           if(this.nameInput.match(str) ) {
-             console.log("this is a string")
-           }
-   
-           if(this.numberInput.match(phoneNumber)){
-             console.log("this phone number is correct")
-           }
+
+      validations(fields, regex){
+        if(regex.test(fields.value)){
+       fields.className = "valid"
+        }
+        else{
+          fields.className = "invalid"
+        }
+
+      
+      }
+
+
+    ,
+
+    
+    reversedLessons: function() {
+    
+      if(this.lessons.length > 0){
+        let lessonArray = this.lessons.slice(0);
+
+        if(this.sortFeature === "subject"){
+          console.log("hi");
+        function compare(a, b){
+          if(a.subject.toLowerCase() < b.subject.toLowerCase())
+            return -1
+            console.log("hi 1");
+          if(a.subject.toLowerCase() > b.subject.toLowerCase())
+            return 1
+            console.log("hi 2");
+          return 0
+        }
+  
+        
+        return lessonArray.sort(compare)
+      }
+
+     
+
+
+  
+
+
+
+      
+
+
+      }
+
+    },
+
+  
+      checkBtn(){
+
+        let patterns = {
+          telephone : /^[/d{11}]$/,
+          strings : /^[a-zA-Z]$/ 
+  
+        }
+
+
+        
+        if(this.nameInput.length != 0 && this.numberInput.length != 0){
+          this.message = "Complete"; 
+             document.getElementById("naming").addEventListener("keyup", names);
+          
+
+        function names(){
+          let telephone = /^\d{11}$/
+          let tel2 = /^0[0-9]{11}$/
+          let strings =  /[a-zA-Z]/g
+          let x = document.getElementById("naming");
+          let y = document.getElementById("numbering");
+          
+
+          
+          if(strings.test(x.value) && telephone.test(y.value) ){
+            x.style.backgroundColor = "green";
+            y.style.backgroundColor = "green";
+            x.style.color = "white";
+            y.style.color = "white";
+            alert("Correct Details");
+            
+          }
+          else{
+            x.style.backgroundColor = "red";
+            y.style.backgroundColor = "red";
+            x.style.color = "white";
+            y.style.color = "white";
+          }
+
+         /* if(telephone.test(x.value)){
+            console.log("trues")
+          }
+          else if(strings.test(x.value)){
+     console.log("it's a string")
+          }
+
+          if(telephone.test(y.value)){
+            console.log("trues 2")
+          }
+          else if(strings.test(y.value)){
+     console.log("it's a string 2")
+          } */
+
+          if(strings.test(x.value) && telephone.test(y.value) ){
+            console.log("correct form")
+          }
+
+        } 
+       
+
+        names()
+          
         }
         else{
           this.message = "Fill in all the fields"
@@ -119,11 +239,16 @@ filters: {
           })
 
           
-
         }
 
 
-        if(this.lessons.length > 0){
+   
+  
+
+
+        // second option
+
+       /* if(this.lessons.length > 0){
           let lessonArray = this.lessons.slice(0);
 
           if(this.sortFeature === "subject"){
@@ -183,10 +308,72 @@ filters: {
         
 
 
+        } */
+
+
+        if (this.sortOption === "ascending") {
+          
+          switch (this.sortFeature) {
+            case "subject":
+              return lessons.sort((a, b) => {
+                if (a.subject.toLowerCase() < b.subject.toLowerCase()) return -1;
+                return 1;
+              });
+            case "location":
+              return lessons.sort((a, b) => {
+                if (a.location.toLowerCase() < b.location.toLowerCase())
+                  return -1;
+                return 1;
+              });
+            case "price":
+              return lessons.sort((a, b) => a.price - b.price);
+            case "availability":
+              return lessons.sort(
+                (a, b) => a.spaces - a.count - (b.spaces - b.count)
+              );
+          }
+        }else if(this.sortOption === "descending"){
+          switch (this.sortFeature) {
+            case "subject":
+              return lessons.sort((a, b) => {
+                if (a.subject.toLowerCase() < b.subject.toLowerCase()) return 1;
+                return -1;
+              });
+            case "location":
+              return lessons.sort((a, b) => {
+                if (a.location.toLowerCase() < b.location.toLowerCase()) return 1;
+                return -1;
+              });
+            case "price":
+              return lessons.sort((a, b) => b.price - a.price);
+            case "availability":
+              return lessons.sort(
+                (a, b) => (b.spaces - b.count )- (a.spaces - a.count)
+              );
+          }
         }
 
+
+
+
         return eachLesson
-   
+   }
+
+
+
+
+
+        }
+
+
+       
+
+
+
+        
+
+
+       
 
 
     
@@ -194,11 +381,16 @@ filters: {
        //returns this.lessons array and uses the filter method , to filter each lesson item and return if the title lesson exists it should be true, if it doesn't exist,it shoulbe be false
        //returns an updated array to be looped over 
       },
+
+
+      
+
+      
      
      /* canAddToCart: function () {
         return this.lessons.spaces > this.cartItem;
       }, */
 
-    }
+    
 
-})
+    )
