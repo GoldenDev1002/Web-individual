70d3c4321c3bf23c4ba3005915b9a4d59f263b21
GoldenDev1002 <jenzielol1002@gmail.com>
2022-11-17 20:22:32 +0000

    updated go to cart button

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-let vueApp = document.getElementById("newApp"); // it will run first and get the element with new app which is assigned to vueApp varaible and placed in vue instance
-import { lessons } from "./lessons.js";
 
-let app = new Vue(
-  {
+let vueApp = document.getElementById("newApp"); // it will run first and get the element with new app which is assigned to vueApp varaible and placed in vue instance
+import {lessons} from "./lessons.js"
+
+let app = new Vue({
     el: vueApp,
     data() {
-      return {
-        //key is a unique identifier for each of the object  that maps
+      return { //key is a unique identifier for each of the object  that maps 
         sitename: "Vue.js School Project",
-
-        /* lesson: {
+        
+       /* lesson: {
           id: 1000,
           subject: "English",
           location: "Hendon",
@@ -23,41 +22,46 @@ let app = new Vue(
           phoneNumber: "",
         },*/
 
+
         lessons, //is a state in vue
         cart: [],
         showContent: true,
-        nameInput: "",
+        nameInput : "",
         numberInput: "",
         message: "",
-        search: "",
+        search : "",
         sortFeature: "",
         sortOption: "",
-        ascending: "",
-        descending: "",
-        cartIcon: "fa fa-cart-shopping",
+        ascending : "",
+        descending : "",
+        cartIcon: "fa fa-cart-shopping"
+
+       
+      
       };
     },
-    filters: {},
+filters: {
+
+},
     methods: {
       addToCart(aLesson) {
         // reduces number of spaces for lesson given
         aLesson.spaces--;
 
         // check if lesson passed is already existing in cart
-        var existingItemInCart = this.cart.find(
-          (item) => item.lessonId === aLesson.id
-        );
-        if (existingItemInCart == null) {
+        var existingItemInCart = this.cart.find(item => item.lessonId === aLesson.id);
+        if(existingItemInCart == null){
           // lesson does not yet exist in cart, add lesson to cart
           // create and save cartItem object for new lesson to be added to cart
-          var cartItem = { lessonId: aLesson.id, spaces: 1, lesson: aLesson };
+          var cartItem = {lessonId : aLesson.id, spaces: 1, lesson: aLesson};
           this.cart.push(cartItem);
-        } else {
+        }else{
           // update number of spaces for existing lesson in cart
           ++existingItemInCart.spaces;
         }
       },
 
+    
       showButton() {
         this.showContent = this.showContent ? false : true;
       },
@@ -66,91 +70,242 @@ let app = new Vue(
         return aLesson.spaces > this.cartCount(aLesson.id);
       },
 
-      cartCount(lesson) {
+      cartCount(lesson){
         let count = 0;
-        for (var i = 0; i < this.cart.length; i++) {
-          if (this.cart[i] === lesson) {
-            count++;
+        for(var i = 0; i < this.cart.length; i++){
+          if(this.cart[i]=== lesson){
+            count++
           }
         }
 
         return count;
       },
 
-      removeCart(newCartItem) {
 
-        this.cart.splice(this.cart.indexOf(newCartItem), 1);
-
-        var existingItemInCart = this.cart.find(
-          (item) => item.id === newCartItem.id
-        );
-        console.log(existingItemInCart);
-        lessons.spaces++;
-      },
-
-      checkBtn() {
-
-        if (this.nameInput.length != 0 && this.numberInput.length != 0) {
-          this.message = "Complete";
-          document.getElementById("naming").addEventListener("keyup", names);
-
-          function names() {
-            let telephone = /^\d{11}$/;
-            let tel2 = /^((\+44)|(0)) ?\d{4} ?\d{6}$/;
-            let strings = /[a-zA-Z]/g;
-            let x = document.getElementById("naming");
-            let y = document.getElementById("numbering");
-            console.log("Correct");
-
-            if (strings.test(x.value) && tel2.test(y.value)) {
-              x.style.backgroundColor = "green";
-              y.style.backgroundColor = "green";
-              x.style.color = "white";
-              y.style.color = "white";
-
-              alert("Form submitted");
-              console.log("Correct3");
-            } else {
-              x.style.backgroundColor = "red";
-              y.style.backgroundColor = "red";
-              x.style.color = "white";
-              y.style.color = "white";
-            }
-
-     
-          }
-
-          names();
-        } else {
-          this.message = "Fill in all the fields";
+      validations(fields, regex){
+        if(regex.test(fields.value)){
+       fields.className = "valid"
         }
-      },
-    },
-
-    computed: {
-      cartItem: function () {
-        return this.cart.length || ""; //this data is unsuitabkle because of use rinteraction i.e the user is supposed to input in this scenario
-      },
-
-      filteredLessons: function () {
-        let eachLesson = this.lessons;
-        if (this.search != "") {
-          eachLesson = eachLesson.filter((lesson) => {
-            return (
-              lesson.subject.toLowerCase().match(this.search) ||
-              lesson.location.toLowerCase().match(this.search)
-            );
-          });
+        else{
+          fields.className = "invalid"
         }
 
       
+      }
+    ,
+
+    removeCart(lessonId){
+      // find selected lesson in cart
+      var itemInCart = this.getCartItemFromCartByLessonId(lessonId);
+
+      if(itemInCart.spaces == 1){
+          // if just one item space is left, remove item completely from cart
+          var index = this.cart.map(x => x.lessonId).indexOf(lessonId);
+          this.cart.splice(index, 1);
+
+          // redirect user back to home if cart is empty
+          if(this.cart.length <= 0){
+            
+            this.showButton();
+          }
+      }else{
+          // reduce number of spaces of item in cart
+          --itemInCart.spaces;
+      }
+
+      // increase lesson space 
+      var lesson = this.getLessonById(lessonId);
+      ++lesson.spaces;
+  },
+  // get lesson by id
+  getLessonById(lessonId){
+      var lesson = this.lessons.find(u => u.id == lessonId);
+      return lesson;
+  },
+  // get item in cart by id
+  getCartItemFromCartByLessonId(lessonId){
+      var item = this.cart.find(u => u.lessonId == lessonId);
+      return item;
+  },
+
+    
+  
+
+  
+      checkBtn(){
+
+        let patterns = {
+          telephone : /^[/d{11}]$/,
+          strings : /^[a-zA-Z]$/ 
+  
+        }
+
+
+        
+        if(this.nameInput.length != 0 && this.numberInput.length != 0){
+          this.message = "Complete"; 
+             document.getElementById("naming").addEventListener("keyup", names);
+          
+
+        function names(){
+          let telephone = /^\d{11}$/
+          let tel2 = /^((\+44)|(0)) ?\d{4} ?\d{6}$/
+          let strings =  /[a-zA-Z]/g
+          let x = document.getElementById("naming");
+          let y = document.getElementById("numbering");
+          console.log("Correct")
+
+          
+          if(strings.test(x.value) && tel2.test(y.value)){
+            x.style.backgroundColor = "green";
+            y.style.backgroundColor = "green";
+            x.style.color = "white";
+            y.style.color = "white";
+           
+            alert("Form submitted");
+            console.log("Correct3")
+
+           
+            
+          }
+          else{
+            x.style.backgroundColor = "red";
+            y.style.backgroundColor = "red";
+            x.style.color = "white";
+            y.style.color = "white";
+           
+          }
+
+         /* if(telephone.test(x.value)){
+            console.log("trues")
+          }
+          else if(strings.test(x.value)){
+     console.log("it's a string")
+          }
+
+          if(telephone.test(y.value)){
+            console.log("trues 2")
+          }
+          else if(strings.test(y.value)){
+     console.log("it's a string 2")
+          } */
+
+          
+
+        } 
+       
+
+        names()
+          
+        }
+        else{
+          this.message = "Fill in all the fields"
+        }
+        },
+
+
+
+        
+      
+    },
+
+    
+    computed: {
+      cartItem: function () {
+        return this.cart.length; //this data is unsuitabkle because of use rinteraction i.e the user is supposed to input in this scenario
+      },
+
+
+     
+
+      filteredLessons : function(){
+        let eachLesson = this.lessons;
+        if(this.search != ""){
+          eachLesson = eachLesson.filter((lesson) => {
+
+            return lesson.subject.toLowerCase().match(this.search) || lesson.location.toLowerCase().match(this.search)
+          })
+
+          
+        }
+
+
+   
+  
+
+
+        // second option
+
+       /* if(this.lessons.length > 0){
+          let lessonArray = this.lessons.slice(0);
+
+          if(this.sortFeature === "subject"){
+          function compare(a, b){
+            if(a.subject.toLowerCase() < b.subject.toLowerCase())
+              return -1
+            
+            if(a.subject.toLowerCase() > b.subject.toLowerCase())
+              return 1
+            
+            return 0
+          }
+
+          return lessonArray.sort(compare)
+        }
+
+        else if(this.sortFeature === "price"){
+
+          let lessonArray = this.lessons.slice(0);
+
+          function compare(a, b){
+            return a.price - b.price 
+          }
+
+          return lessonArray.sort(compare)
+            
+         
+        }
+
+
+        else if(this.sortFeature === "location"){
+
+          if(this.lessons.length > 0){
+            let lessonArray = this.lessons.slice(0);
+
+          function compare(a, b){
+            if(a.location.toLowerCase() < b.location.toLowerCase())
+              return -1
+            
+            if(a.location.toLowerCase() > b.location.toLowerCase())
+              return 1
+            
+            return 0
+          }
+
+          return lessonArray.sort(compare)
+
+        }
+      
+        }
+
+
+    
+
+
+
+        
+
+
+        } */
+
 
         if (this.sortOption === "ascending") {
+          
           switch (this.sortFeature) {
             case "subject":
               return lessons.sort((a, b) => {
-                if (a.subject.toLowerCase() < b.subject.toLowerCase())
-                  return -1;
+                if (a.subject.toLowerCase() < b.subject.toLowerCase()) 
+                return -1;
                 return 1;
               });
             case "location":
@@ -162,43 +317,76 @@ let app = new Vue(
             case "price":
               return lessons.sort((a, b) => a.price - b.price);
             case "availability":
-              return lessons.sort((a, b) => {
-                if (a.spaces > b.spaces) return -1;
-                return 1;
-              });
+              return lessons.sort(
+                (a, b) => {
+                  if(a.spaces >b.spaces) return -1;
+                  return 1
+                }
+              );
           }
-        } else if (this.sortOption === "descending") {
+        }else if(this.sortOption === "descending"){
           switch (this.sortFeature) {
             case "subject":
               return lessons.sort((a, b) => {
-                if (a.subject.toLowerCase() < b.subject.toLowerCase()) return 1;
+                if (a.subject.toLowerCase() < b.subject.toLowerCase()) 
+                return 1;
                 return -1;
               });
             case "location":
               return lessons.sort((a, b) => {
-                if (a.location.toLowerCase() < b.location.toLowerCase())
-                  return 1;
+                if (a.location.toLowerCase() < b.location.toLowerCase()) return 1;
                 return -1;
               });
             case "price":
               return lessons.sort((a, b) => b.price - a.price);
             case "availability":
-              return lessons.sort((a, b) => {
-                if (a.spaces > b.spaces) return 1;
-                return -1;
-              });
+              return lessons.sort(
+                (a, b) => {
+                  if(a.spaces > b.spaces) return 1;
+                  return -1
+                }
+              );
           }
         }
 
-        return eachLesson;
+
+
+
+        return eachLesson
+   }
+
+
+
+
+
+        }
+
+
+       
+
+
+
+        
+
+
+       
+
+
+    
+      
+       //returns this.lessons array and uses the filter method , to filter each lesson item and return if the title lesson exists it should be true, if it doesn't exist,it shoulbe be false
+       //returns an updated array to be looped over 
       },
-    },
 
-    //returns this.lessons array and uses the filter method , to filter each lesson item and return if the title lesson exists it should be true, if it doesn't exist,it shoulbe be false
-    //returns an updated array to be looped over
-  }
 
-  /* canAddToCart: function () {
+      
+
+      
+     
+     /* canAddToCart: function () {
         return this.lessons.spaces > this.cartItem;
       }, */
-);
+
+    
+
+    )
