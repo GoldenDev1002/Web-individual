45032341bc2103f46a378f6aa1f4980b12537e02
GoldenDev1002 <jenzielol1002@gmail.com>
2022-11-17 18:20:18 +0000

    updated main js 2

--- main.js
+++ main.js
@@ -33,7 +33,8 @@ let app = new Vue({
         sortFeature: "",
         sortOption: "",
         ascending : "",
-        descending : ""
+        descending : "",
+        cartIcon: "fa fa-cart-shopping"
 
        
       
@@ -95,41 +96,37 @@ filters: {
 
     ,
 
-    
-    reversedLessons: function() {
-    
-      if(this.lessons.length > 0){
-        let lessonArray = this.lessons.slice(0);
 
-        if(this.sortFeature === "subject"){
-          console.log("hi");
-        function compare(a, b){
-          if(a.subject.toLowerCase() < b.subject.toLowerCase())
-            return -1
-            console.log("hi 1");
-          if(a.subject.toLowerCase() > b.subject.toLowerCase())
-            return 1
-            console.log("hi 2");
-          return 0
-        }
-  
-        
-        return lessonArray.sort(compare)
-      }
-
+    removeCart(newCartItem){
      
-
-
-  
-
-
-
+     /* var existingItemInCart = this.cart.find(item => item.lessonId === newCartItem.id);
+      if(existingItemInCart){
+      if(newCartItem.spaces >= 1){
+                      this.cart.splice(this.cart.indexOf(newCartItem), 1);
       
+                  }else{
+                      --newCartItem.spaces;
+                  }
+      
+                  ++lessons.spaces;
 
 
-      }
+
+                }
+      else{
+        console.log("item doesn't exist")
+      } */
+
+      this.cart.splice(this.cart.indexOf(newCartItem), 1);
+      
+      var existingItemInCart = this.cart.find(item => item.id === newCartItem.id);
+      console.log(existingItemInCart);
+      lessons.spaces++
 
     },
+
+    
+  
 
   
       checkBtn(){
@@ -149,19 +146,23 @@ filters: {
 
         function names(){
           let telephone = /^\d{11}$/
-          let tel2 = /^0[0-9]{11}$/
+          let tel2 = /^((\+44)|(0)) ?\d{4} ?\d{6}$/
           let strings =  /[a-zA-Z]/g
           let x = document.getElementById("naming");
           let y = document.getElementById("numbering");
-          
+          console.log("Correct")
 
           
-          if(strings.test(x.value) && telephone.test(y.value) ){
+          if(strings.test(x.value) && tel2.test(y.value)){
             x.style.backgroundColor = "green";
             y.style.backgroundColor = "green";
             x.style.color = "white";
             y.style.color = "white";
-            alert("Correct Details");
+           
+            alert("Form submitted");
+            console.log("Correct3")
+
+           
             
           }
           else{
@@ -169,6 +170,7 @@ filters: {
             y.style.backgroundColor = "red";
             x.style.color = "white";
             y.style.color = "white";
+           
           }
 
          /* if(telephone.test(x.value)){
@@ -185,9 +187,7 @@ filters: {
      console.log("it's a string 2")
           } */
 
-          if(strings.test(x.value) && telephone.test(y.value) ){
-            console.log("correct form")
-          }
+          
 
         } 
        
@@ -213,22 +213,7 @@ filters: {
       },
 
 
-      sortedLessons: function(){
-        if(this.lessons.length > 0){
-          let lessonArray = this.lessons.slice(0);
-          function compare(a, b){
-            if(a.subject.toLowerCase() < b.subject.toLowerCase())
-              return -1
-            
-            if(a.subject.toLowerCase() > b.subject.toLowerCase())
-              return 1
-            
-            return 0
-          }
-
-          return lessonArray.sort(compare)
-        }
-      },
+     
 
       filteredLessons : function(){
         let eachLesson = this.lessons;
@@ -316,7 +301,8 @@ filters: {
           switch (this.sortFeature) {
             case "subject":
               return lessons.sort((a, b) => {
-                if (a.subject.toLowerCase() < b.subject.toLowerCase()) return -1;
+                if (a.subject.toLowerCase() < b.subject.toLowerCase()) 
+                return -1;
                 return 1;
               });
             case "location":
@@ -329,14 +315,18 @@ filters: {
               return lessons.sort((a, b) => a.price - b.price);
             case "availability":
               return lessons.sort(
-                (a, b) => a.spaces - a.count - (b.spaces - b.count)
+                (a, b) => {
+                  if(a.spaces >b.spaces) return -1;
+                  return 1
+                }
               );
           }
         }else if(this.sortOption === "descending"){
           switch (this.sortFeature) {
             case "subject":
               return lessons.sort((a, b) => {
-                if (a.subject.toLowerCase() < b.subject.toLowerCase()) return 1;
+                if (a.subject.toLowerCase() < b.subject.toLowerCase()) 
+                return 1;
                 return -1;
               });
             case "location":
@@ -348,7 +338,10 @@ filters: {
               return lessons.sort((a, b) => b.price - a.price);
             case "availability":
               return lessons.sort(
-                (a, b) => (b.spaces - b.count )- (a.spaces - a.count)
+                (a, b) => {
+                  if(a.spaces > b.spaces) return 1;
+                  return -1
+                }
               );
           }
         }
---
+++ readme.md
@@ -0,0 +1,3 @@
+# https://github.com/GoldenDev1002/Web-individual
+
+#
